d17c5be7f59907809cd56c40a772bb609691ec39
Megan Neyer <Megan.Neyer@gmail.com>
2021-03-23 20:53:13 -0400

    functional and class component props

--- red-badge-demo-ts/src/components/ClassComponentProps.jsx
+++ red-badge-demo-ts/src/components/ClassComponentProps.jsx
@@ -1,2 +1,56 @@
 //Create a class component based on the functionalProps.jsx file
 //Keep the usage of destructuring props in this challenge.
+
+import React, { Component } from 'react';
+import { Alert, Container } from "reactstrap"
+
+class ClassComponentProps extends Component{
+    displayAlerts(){
+        const {numbers} = this.props;
+        return numbers.map((num, index) => (
+            <Alert key={index} color={num % 2 === 0 ? "success" : "danger"}>
+            The number is {num}{" "}
+            </Alert>        
+        ));
+    }
+    render() {
+        return(
+            <div>
+                <Container>
+                    <h3>Changed to Class Component Props</h3>
+                    {this.displayAlerts()}
+                </Container>
+            </div>
+        )
+    }
+}
+
+export default ClassComponentProps;
+
+
+//2 different ways to make the same thing happen
+//OPTION TWO
+
+// import React, { Component } from 'react';
+// import { Alert, Container } from "reactstrap"
+
+// class ClassComponentProps extends Component{
+//     render(){
+//     const {numbers} = this.props;
+//     const displayAlerts = () => numbers.map((num, index) => 
+//         <Alert key={index} color={num % 2 === 0 ? "success" : "danger"}>
+//             The number is {num}{" "}
+//         </Alert>        
+//     )
+//         return(
+//             <div>
+//                 <Container>
+//                     <h3>Changed to Class Component Props</h3>
+//                     {displayAlerts()}
+//                 </Container>
+//             </div>
+//         )
+//     }
+// }
+
+// export default ClassComponentProps;
