bff9c5b463774f7f5b16bbe9af3201597393a387
Megan Neyer <Megan.Neyer@gmail.com>
2021-03-25 21:26:03 -0400

    challenge fetch

--- red-badge-demo-ts/src/App.tsx
+++ red-badge-demo-ts/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import FunctionFetchOnload from "./components/FunctionFetchOnload";
+import ClassFetchOnLoad from "./components/ClassFetchOnload"
 
 function App() {
   return (
     <div>
       <FunctionFetchOnload />
+      <hr />
+      <ClassFetchOnLoad />
     </div>
   );
 }
