34b55a748fe10183ee6a19602ad0345822fac1fe
Megan Neyer <Megan.Neyer@gmail.com>
2021-03-27 16:11:17 -0400

    dice game

---
+++ red-badge-demo-ts/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export interface FooterProps {
+    
+}
+
+const Footer: React.SFC<FooterProps> = () => {
+    return (
+        <div>
+            Let them Roll 🎲🎲
+        </div>
+    );
+}
+
+export default Footer;
---
+++ red-badge-demo-ts/src/components/MainPage.tsx
@@ -0,0 +1,44 @@
+import * as React from 'react';
+import { Component } from 'react';
+import RollButton from './Button';
+import Dice from "./Dice"
+
+export interface MainPageProps {
+    
+}
+
+export interface MainPageState {
+    headerText : string;
+    diceRoll1: number;
+    diceRoll2: number;
+}
+
+class MainPage extends React.Component<MainPageProps, MainPageState> {
+    constructor(props: MainPageProps) {
+        super(props);
+        this.state = { headerText : "", diceRoll1: 3, diceRoll2: 5 };
+    }
+
+    //how to update a setState to a child from a parent
+    rollDiceOne = (roll: number) => {
+        this.setState({diceRoll1: roll})
+    }
+
+    rollDiceTwo = (roll: number) => {
+        this.setState({diceRoll2: roll})
+    }
+
+    render() { 
+        return (
+            <div>
+                <p>{this.state.headerText}</p>
+                <br />
+                <Dice playerName="Player 1" diceRoll={this.state.diceRoll1}/>
+                <Dice playerName="Player 2" diceRoll={this.state.diceRoll2}/>
+                <RollButton rollDiceOne={this.rollDiceOne} rollDiceTwo={this.rollDiceTwo}/>
+            </div>
+        );
+    }
+}
+
+export default MainPage;
